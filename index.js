b23969c2bc48172952066b82f9613473a82223b8
Lyutink <krivenko.lyuda@gmail.com>
2022-05-23 15:30:33 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
 const contactsOperations = require("./contacts");
-
-const fs = require("fs/promises");
-
 
 const invokeAction = async ({ action, id, name, email, phone}) => {
     switch (action) {
@@ -29,41 +29,10 @@ const invokeAction = async ({ action, id, name, email, phone}) => {
     }
 }
 
-//const id = "1";
-// const newData = {
-//     "name": "Qqqqq Ttttt",
-//     "email": "////////",
-//     "phone": "(888) 888-3792"
-// };
-const contactId = "11";
-//invokeAction({ action: "list" });
-//invokeAction({ action: "get", id });
-//invokeAction({ action: "add", name: "Qqqqq Ttttt", email: "////////", phone: "(888) 888-3792" });
-invokeAction({action: "remove", id: contactId });
 
-// const fileOperation = async (filePath, action = "read", data = "") => {
-//     switch (action) {
-//         case "read":
-//             const text = await fs.readFile(filePath, "utf-8");
-//             console.log(text);
-//             break;
-//         case "add":
-//             await fs.appendFile(filePath, data)
-//             break;
-//         case "replace":
-//             await fs.writeFile(filePath, data);
-//             break;
-//         default:
-//             console.log("Unknown action")
-//     }
-// }
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr).option('id', {type:'string'});
+console.log(argv);
 
-// fileOperation("db/tot.txt");
-// //fileOperation("db/tot.txt", "add", "\nAnd you Helloooo!!!")
-// fileOperation("db/tot.txt", "replace", "And you Helloooo!!!")
-// fs.readFile("db/tot.txt", "utf-8")
-//     .then(data => {
-//         //const text = data.toString();
-//         console.log(data)
-//     })
-//     .catch(error => console.log(error.message))
+invokeAction(argv);
+
